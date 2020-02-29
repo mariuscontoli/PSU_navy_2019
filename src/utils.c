684eaf0ae12fdb24182c5a0ea1cb3af746be3006
marius <marius.contoli-legay@epitech.eu>
2020-02-29 13:07:05 +0100

    added utilitary functions

--- src/utils.c
+++ src/utils.c
@@ -7,6 +7,20 @@
 
 #include "../include/navy.h"
 
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void my_putstr(char const *str)
+{
+    int i = 0;
+    while (str[i] != '\0') {
+        my_putchar(str[i]);
+        i++;
+    }
+}
+
 int my_strlen(char *str)
 {
     int len;
