6ea0e833f0bb975fe2750afecad5bf76d2b34d15
marius <marius.contoli-legay@epitech.eu>
2020-02-29 13:05:58 +0100

    help file

---
+++ src/my_help.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** my_help.c
+** File description:
+** display help for navy project
+*/
+
+#include "../include/navy.h"
+
+void display_help(void)
+{
+    my_putstr("USAGE\n");
+    my_putstr("\t./navy [first_player_pid] navy_positions\n");
+    my_putstr("DESCRIPTION\n");
+    my_putstr("\tfirst_player_pid: only for the 2nd player. pid of the first player.\n");
+    my_putstr("\tnavy_positions: file representing the positions of the ships.\n");
+}
