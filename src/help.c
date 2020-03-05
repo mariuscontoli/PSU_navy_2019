28d1b5305f486af0cb11c2f1204bacb66516dfcf
marius <marius.contoli-legay@epitech.eu>
2020-03-05 11:18:41 +0100

    coding style push

--- src/help.c
+++ src/help.c
@@ -12,6 +12,8 @@ void display_help(void)
     my_putstr("USAGE\n");
     my_putstr("\t./navy [first_player_pid] navy_positions\n");
     my_putstr("DESCRIPTION\n");
-    my_putstr("\tfirst_player_pid: only for the 2nd player. pid of the first player.\n");
-    my_putstr("\tnavy_positions: file representing the positions of the ships.\n");
+    my_putstr("\tfirst_player_pid: only for the 2nd player."
+    " pid of the first player.\n");
+    my_putstr("\tnavy_positions: file representing the positions"
+    " of the ships.\n");
 }
